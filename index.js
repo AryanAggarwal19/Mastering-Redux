a3a3af4b1d910b506c09f463f7d25f50b6ddc1d1
Aryan Agarwal <aryanaggarwal1901@gmail.com>
2024-01-13 05:16:23 -0800

    Middlewares, ActionTypes

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
         // Chapter - One (Actions, Reducers, Middlewares)
-import { createStore } from "redux"
+import { createStore, applyMiddleware } from "redux"
+import logger from "redux-logger";
 
 //store setup
-const store = createStore(reducer);
+const store = createStore(reducer,applyMiddleware(logger.default));
 
 const history=[];
 
@@ -14,7 +15,14 @@ if(action.type==='increment'){
 
     return {amount:state.amount+1} //(right practice)
     }
-    return state; //newstate
+    // return state; //newstate
+
+if(action.type==='decrement'){
+    return {amount: state.amount-1 };
+}
+if(action.type==='incrementByAmount'){ //use of payload
+    return {amount:state.amount+ action.payload};
+}
 }
 
 //global state
@@ -34,7 +42,7 @@ if(action.type==='increment'){
 //store.dispatch({type:'increment'})
 
 setInterval(()=>{
-    store.dispatch({type:'increment'})
+    store.dispatch({type:'incrementByAmount', payload:4})
 },2000);
 
 
